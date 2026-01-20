1888da5b36503c2d915ab69b65e376df818a853b
HusainAlmisbah <husain.almisbah@gmail.com>
2026-01-20 20:45:16 +0300

    isAdmin

--- models/User.js
+++ models/User.js
@@ -9,6 +9,11 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  isAdmin: {
+    type: Boolean,
+    required: true,
+    default: false,
+  }
 });
 
 const User = mongoose.model("User", userSchema);
